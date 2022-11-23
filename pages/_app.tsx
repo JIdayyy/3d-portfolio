511bf57b902247b641d137196f30d0a3a616f19e
JIdayyy <julien_abbadie@hotmail.fr>
2022-11-23 19:22:00 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,7 @@ import "@fontsource/akshar/400.css";
 import "@fontsource/akshar/500.css";
 import "@fontsource/akshar/600.css";
 import "@fontsource/akshar/700.css";
-import CustomCursor from "custom-cursor-react";
-import "custom-cursor-react/dist/index.css";
+// import "custom-cursor-react/dist/index.css";
 import theme from "../src/definitions/chakra/theme";
 import { TransitionProvider } from "../src/context/Transition";
 import TransitionLayout from "../src/components/Layout/TransitionLayout";
@@ -19,7 +18,7 @@ import TransitionLayout from "../src/components/Layout/TransitionLayout";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <CustomCursor
+      {/* <CustomCursor
         targets={[".link", ".your-css-selector"]}
         customClass="custom-cursor"
         dimensions={30}
@@ -30,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
           opacity: 0.2,
         }}
         targetOpacity={0.5}
-      />
+      /> */}
       <TransitionProvider>
         <TransitionLayout>
           <Layout>
